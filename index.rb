d48318a6f7c363f5bea5039f7fb4e42e888fada6
屋比久拓海 <yabikutakumi@yabikutakuminoMacBook-puro.local>
2019-07-18 20:09:43 +0900

    犬クラス定義した

--- index.rb
+++ index.rb
@@ -1,9 +1,40 @@
-sum = 0
-input = gets.to_i
-(1..input).each do |num| 
-  # 2のヒント: この↓のコードを、条件分岐の中に入れてあげる(num % 2 == 0)...？
-  if num % 2 ==0
-  sum += num
+class Dog
+  def initialize(type, name, owner, sex)
+    @type = type
+    @name = name
+    @owner = owner
+    @sex = sex
+    introduction()
+  end
+
+  def introduction()
+    puts "ワンワン！(僕、#{@name}!)"
+    puts "ワンワン！(犬種は#{@type}で、性別は#{@sex}だよ!)"
+  end
+
+  def bark(count)
+    count.times do |num|
+      print 'ワン'
+    end
+    puts ''
+  end
+
+  def run(distance)
+    puts "ワン!(僕、#{distance}m走ったよ！)"
+  end
+
+  def eat(food)
+    if food == '骨'
+      puts 'ワン!(美味しい!)'
+    elsif food == 'ステーキ'
+      puts "ワンワン!(#{@owner}さん！最高！)"
+    else
+      puts '...ﾎﾞﾘﾎﾞﾘ'
+    end
   end
 end
-puts sum
+
+hachi = Dog.new('ミニチュアピンシャア', 'メイちゃん', '中島さん', 'めす')
+hachi.bark(1)
+hachi.run(100)
+hachi.eat('ステーキ')
